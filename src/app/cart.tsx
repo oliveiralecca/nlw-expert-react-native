e83235f80c3a079de2f93378f8df45c9dd21b8fd
oliveiralecca <oliveiraslc@yahoo.com.br>
2024-02-12 16:14:46 -0300

    feat(nlw-expert): creates cart component

---
+++ src/app/cart.tsx
@@ -0,0 +1,113 @@
+import { Button } from "@/components/button";
+import { Header } from "@/components/header";
+import { Input } from "@/components/input";
+import { LinkButton } from "@/components/link-button";
+import { Product } from "@/components/product";
+import { ProductCartProps, useCartStore } from "@/stores/cart-store";
+import { formatCurrency } from "@/utils/functions/format-currency";
+import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "expo-router";
+import { useState } from "react";
+import { View, Text, ScrollView, Alert, Linking } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+
+// o componente `ScrollView` é uma `View` com o scroll ativado
+// o componente `KeyboardAwareScrollView`, da lib de mesmo nome, serve para contornar o comportamento do iOS em que o teclado quando é aberto, não empurra o conteúdo para cima
+// o componente `Linking` serve para abrir um link pelo RN
+
+const PHONE_NUMBER = "557963425612";
+
+export default function Cart() {
+  const [address, setAddress] = useState("");
+  
+  const cartStore = useCartStore();
+  const navigation = useNavigation();
+
+  const total = formatCurrency(
+    cartStore.products.reduce(
+      (total, product) => (total + product.price * product.quantity), 
+      0
+    )
+  );
+
+  function handleProductRemove(product: ProductCartProps) {
+    Alert.alert("Remover", `Deseja remover ${product.title} do carrinho?`, [
+      { text: "Cancelar" },
+      { text: "Remover", onPress: () => cartStore.remove(product.id) }
+    ])
+  }
+
+  function handleOrder() {
+    if (address.trim().length === 0) {
+      return Alert.alert("Pedido", "Informe os dados da entrega.")
+    }
+
+    const products = cartStore.products.map((product) => `\n ${product.quantity} ${product.title}`).join("");
+    
+    const message = `
+    🍔 NOVO PEDIDO
+    \n Entregar em: ${address}
+
+    ${products}
+
+    \n Valor total: ${total}
+    `;
+
+    Linking.openURL(`http://api.whatsapp.com/send?phone=${PHONE_NUMBER}&text=${message}`)
+
+    cartStore.clear();
+    navigation.goBack();
+  }
+
+  return (
+    <View className="flex-1 pt-8">
+      <Header title="Seu carrinho" />
+
+      <KeyboardAwareScrollView>
+        <ScrollView>
+          <View className="p-5 flex-1">
+            {cartStore.products.length > 0 ? (
+              <View className="border-b border-slate-700"> 
+                {cartStore.products.map((product) => (
+                  <Product
+                    key={product.id}
+                    data={product}
+                    onPress={() => handleProductRemove(product)}
+                  />
+                ))}
+              </View>
+            ) : (
+              <Text className="font-body text-slate-400 text-center my-8">
+                Seu carrinho está vazio.
+              </Text>
+            )}  
+
+            <View className="flex-row gap-2 items-center mt-5 mb-4">
+              <Text className="text-white text-xl font-subtitle">Total:</Text>
+              <Text className="text-lime-400 text-2xl font-heading">{total}</Text>
+            </View>  
+
+            <Input
+              placeholder="Informe o endereço de entrega com rua, bairro, CEP, número e complemento..."
+              onChangeText={setAddress}
+              blurOnSubmit
+              onSubmitEditing={handleOrder}
+              returnKeyType="next"
+            />
+          </View>
+        </ScrollView> 
+      </KeyboardAwareScrollView>
+
+      <View className="p-5 gap-5">
+        <Button onPress={handleOrder}>
+          <Button.Text>Enviar pedido</Button.Text>
+          <Button.Icon>
+            <Feather name="arrow-right-circle" size={20} />
+          </Button.Icon>
+        </Button>
+
+        <LinkButton title="Voltar ao cardápio" href="/" /> 
+      </View>
+    </View>
+  )
+}
--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -1,8 +1,12 @@
 import { ProductProps } from "@/utils/data/products";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as cartInMemory from "./helpers/cart-in-memory";
 
 // a lib `zustand` serve para criar estados globais, como os contextos do próprio react e a lib `redux`
+// a lib `async-storage` serve para persistir os dados da aplicação, pois sem isso os dados são perdidos ao recarregar o app
+// além da lib acima, é necessário o uso do `createJSONStorage` e `persist` do zustand para persistir os dados na aplicação
 export type ProductCartProps = ProductProps & {
   quantity: number;
 }
@@ -10,13 +14,26 @@ export type ProductCartProps = ProductProps & {
 type StateProps = {
   products: ProductCartProps[];
   add: (product: ProductProps) => void;
+  remove: (productId: string) => void;
+  clear: () => void;
 }
 
-export const useCartStore = create<StateProps>((set) => ({
+export const useCartStore = create(
+  persist<StateProps>((set) => ({
   products: [],
 
   add: (product: ProductProps) =>
     set((state) => ({
       products: cartInMemory.add(state.products, product),
     })),
+
+  remove: (productId: string) =>
+    set((state) => ({
+      products: cartInMemory.remove(state.products, productId)
+    })),
+
+  clear: () => set(() => ({ products: [] }))
+}), {
+  name: "nlw-expert:cart",
+  storage: createJSONStorage(() => AsyncStorage)
 }))
